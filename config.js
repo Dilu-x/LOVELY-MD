fa10a7b40ec46ce78f68496d02a49749cf0b83ef
Dilu-x <avstamilzan15@gmail.com>
2025-07-25 16:49:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "q1AyxJxb#Lq4re3-S0LpQk4fk8RWbU4rbMlGvLrGMj3_e48rjbP0",
+  SESSION_ID: process.env.SESSION_ID || "C0JDgKra#B0DfrsIOPbckhUH0Z8dLBh5iXu8UbwiNCoM1dmk93k8",
   MONGODB: process.env.MONGODB || "mongodb+srv://avstamilzan15:S9bf6vhd9MnoNtTC@cluster0.9vodwx7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   OWNER_NUM: process.env.OWNER_NUM || "94720251446",
 };
